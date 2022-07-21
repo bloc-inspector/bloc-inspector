560ee23a6aa6bf72b69931e30e3968112864186a
Francis <francis94c@gmail.com>
2022-07-22 00:09:57 +0100

    Cleanup.

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,17 @@ import 'package:bloc_inspector/blocs/service/service_bloc.dart';
 import 'package:bloc_inspector/cubits/selected_log_index_cubit.dart';
 import 'package:bloc_inspector/screens/instances.dart';
 import 'package:bloc_inspector/screens/logs.dart';
-import 'package:logger/logger.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    setWindowTitle('My App');
+    setWindowTitle('BLoC Inspector');
     setWindowMaxSize(const Size(double.infinity, double.infinity));
     setWindowMinSize(const Size(1280, 720));
   }
+
   runApp(const MyApp());
 }
 
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter BLoC Investigator',
+        title: 'BLoC Inspector',
         initialRoute: InstancesScreen.routeName,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -47,55 +47,6 @@ class MyApp extends StatelessWidget {
           LogsScreen.routeName: (context) => const LogsScreen(),
         },
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Logger logger = Logger();
-  Map<int, String?> buffer = {};
-
-  void _incrementCounter() async {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
